c30228d36dfa25228e04bd0dad489154c79a6315
Nicolas Widart <n.widart@gmail.com>
2017-10-19 19:15:14 +0200

    Adding mandril info

--- config/services.php
+++ config/services.php
@@ -14,6 +14,10 @@ return [
     |
     */
 
+    'mandrill' => [
+        'secret' => getenv('MANDRILL_API'),
+    ],
+
     'mailgun' => [
         'domain' => env('MAILGUN_DOMAIN'),
         'secret' => env('MAILGUN_SECRET'),
