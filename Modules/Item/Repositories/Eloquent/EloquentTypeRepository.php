68304919112e408ced8a5da13dbaa461b55ed014
Nicolas Widart <n.widart@gmail.com>
2017-10-19 22:21:44 +0200

    Order item types by name

--- Modules/Item/Repositories/Eloquent/EloquentTypeRepository.php
+++ Modules/Item/Repositories/Eloquent/EloquentTypeRepository.php
@@ -7,4 +7,8 @@ use Modules\Core\Repositories\Eloquent\EloquentBaseRepository;
 
 class EloquentTypeRepository extends EloquentBaseRepository implements TypeRepository
 {
+    public function all()
+    {
+        return $this->model->orderBy('name', 'asc')->get();
+    }
 }
